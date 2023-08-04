246eb16da5ce1c14e00aecd702c77a171944bb0b
mtalhaergun <mtalhaergun24@gmail.com>
2023-08-04 16:42:57 +0300

    Update dependencies and hide api key

--- app/src/main/java/com/mte/marvelapp/utils/constants/Constants.kt
+++ app/src/main/java/com/mte/marvelapp/utils/constants/Constants.kt
@@ -1,10 +1,10 @@
 package com.mte.marvelapp.utils.constants
 
+import com.mte.marvelapp.BuildConfig
+
 object Constants {
     const val BASE_URL = "https://gateway.marvel.com/v1/public/"
-//    const val API_KEY = "ecfb1cea717dba51206e941544072ac1"
-//    const val HASH = "a575e33b5fdf1340dde622a62bfaab07"
-    const val API_KEY = "707e535ce465c96c78f9ef1047b55e36"
-    const val HASH = "c64268a690a121043e3d66155f2000b0"
+    const val API_KEY = BuildConfig.API_KEY
+    const val HASH = BuildConfig.HASH
     const val PAGE_SIZE = 20
 }
