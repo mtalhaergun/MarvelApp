3c110f3483a2469ffced3197fafd792240c340d6
mtalhaergun <mtalhaergun24@gmail.com>
2023-08-07 16:23:43 +0300

    Created Config dsl

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,8 @@
+object Config {
+    const val namespace = "com.mte.marvelapp"
+    const val compileSdk = 33
+    const val minSdk = 24
+    const val targetSdk = 33
+    const val versionCode = 2
+    const val versionName = "2.0"
+}
